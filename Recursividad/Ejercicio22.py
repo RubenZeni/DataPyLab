d3ef37ebe31bd700c5c1bc994a6b8d970ae1ee92
RubenZeni <zenirubendario@gmail.com>
2024-05-14 00:32:45 -0300

    Creación de la carpeta Recursividad y, dentro de la misma, de los archivos Ejercicio5 y Ejercicio22, correspondientes al TP1.

---
+++ Recursividad/Ejercicio22.py
@@ -0,0 +1,15 @@
+# El problema de la mochila Jedi. Suponga que un Jedi (Luke Skywalker, Obi-Wan Kenobi, Rey u otro, el que más le guste) está atrapado, pero muy cerca está su mochila que contiene muchos objetos. Implementar una función recursiva llamada “usar la fuerza” que le permita al Jedi “con ayuda de la fuerza” realizar las siguientes actividades:
+# a. sacar los objetos de la mochila de a uno a la vez hasta encontrar un sable de luz o que no queden más objetos en la mochila;
+# b. determinar si la mochila contiene un sable de luz y cuantos objetos fueron necesarios sacar para encontrarlo;
+# c. Utilizar un vector para representar la mochila.
+def usar_la_fuerza(mochila):
+    if not mochila:
+        return ("No quedan objetos en la mochila.")
+    objeto = mochila.pop()
+    if objeto == "sable de luz":
+        return (f"Encontraste el sable de luz. Fue necesario sacar {len(mochila)} objetos para lograrlo.")
+    else:
+        return usar_la_fuerza(mochila)
+    
+mochila = ["esfera de la paz", "anillos de vaale", "mapa estelar", "guía del contrabandista", "sable de luz", "cristal nova", "infante de shaa"]
+print(usar_la_fuerza(mochila))
---
+++ Recursividad/Ejercicio5.py
@@ -0,0 +1,13 @@
+# Desarrollar una función que permita convertir un número romano en un número decimal.
+def romano_a_decimal(romano):
+    valores = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if len(romano) == 0:
+        return 0
+    if len(romano) == 1:
+        return valores[romano]
+    if valores[romano[0]] < valores[romano[1]]:
+        return -valores[romano[0]] + romano_a_decimal(romano[1:])
+    else:
+        return valores[romano[0]] + romano_a_decimal(romano[1:])
+    
+print(f'Número en decimal: {romano_a_decimal(input('Ingrese un número romano: '))}')
