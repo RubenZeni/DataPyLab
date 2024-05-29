9872e2d04f0c6e324cc8a9e34a6e67271be0f322
RubenZeni <zenirubendario@gmail.com>
2024-05-28 22:44:48 -0300

    Corrección del TP2, Ejercicio16. No funcionaba correctamente y estaba mal planteado.

--- Pila/Ejercicio16.py
+++ Pila/Ejercicio16.py
@@ -2,42 +2,59 @@
 
 from pila import Stack
 
-def interseccion_pilas_sin_set(pila1, pila2):
-    auxiliar1 = Stack()
+def interseccion_pilas(pila1, pila2):
+    auxiliar1 = Stack() # Para restaurar 
     auxiliar2 = Stack()
+    auxiliar3 = Stack()  # Para restaurar pila2
     interseccion = Stack()
+    elementos_interseccion = []
 
+    # Mover elementos de pila1 a auxiliar1 (y restaurar pila1)
     while pila1.size() > 0:
         elemento = pila1.pop()
         auxiliar1.push(elemento)
     
+    while auxiliar1.size() > 0:
+        elemento = auxiliar1.pop()
+        pila1.push(elemento)
+        auxiliar2.push(elemento)
+
+    # Verificar la intersección con pila2 y almacenar en intersección
     while pila2.size() > 0:
         elemento = pila2.pop()
+        auxiliar3.push(elemento)  # Guardar elemento de pila2 para restaurar después
         
         esta_en_pila1 = False
-        while auxiliar1.size() > 0:
-            aux_elemento = auxiliar1.pop()
-            auxiliar2.push(aux_elemento)
+        while auxiliar2.size() > 0:
+            aux_elemento = auxiliar2.pop()
+            auxiliar1.push(aux_elemento)
             if aux_elemento == elemento:
                 esta_en_pila1 = True
         
-        while auxiliar2.size() > 0:
-            auxiliar1.push(auxiliar2.pop())
+        # Restaurar auxiliar2
+        while auxiliar1.size() > 0:
+            auxiliar2.push(auxiliar1.pop())
         
-        if esta_en_pila1:
-            interseccion.push(elemento)
+        if esta_en_pila1 and elemento not in elementos_interseccion:
+            elementos_interseccion.append(elemento)
     
-    while interseccion.size() > 0:
-        elemento = interseccion.pop()
-        pila2.push(elemento)
-        pila1.push(elemento)
+    # Restaurar pila2
+    while auxiliar3.size() > 0:
+        pila2.push(auxiliar3.pop())
+
+    # Crear la pila de intersección
+    for elemento in elementos_interseccion:
+        interseccion.push(elemento)
     
     return interseccion
 
 pila_episodio_v = Stack()
 pila_episodio_v.push('Luke Skywalker')
 pila_episodio_v.push('Han Solo')
+pila_episodio_v.push('Chau')
 pila_episodio_v.push('Leia Organa')
+pila_episodio_v.push('Hola')
+pila_episodio_v.push('Kylo Ren')
 pila_episodio_v.push('Darth Vader')
 
 pila_episodio_vii = Stack()
@@ -45,10 +62,19 @@ pila_episodio_vii.push('Rey')
 pila_episodio_vii.push('Finn')
 pila_episodio_vii.push('Leia Organa')
 pila_episodio_vii.push('Han Solo')
+pila_episodio_vii.push('Chau')
 pila_episodio_vii.push('Kylo Ren')
 
-interseccion = interseccion_pilas_sin_set(pila_episodio_v, pila_episodio_vii)
+interseccion = interseccion_pilas(pila_episodio_v, pila_episodio_vii)
 
 print("Personajes en ambos episodios:")
 while interseccion.size() > 0:
     print(interseccion.pop())
+
+print("Personajes en pila de Episodio V:")
+while pila_episodio_v.size() > 0:
+    print(pila_episodio_v.pop())
+
+print("Personajes en pila de Episodio VII:")
+while pila_episodio_vii.size() > 0:
+    print(pila_episodio_vii.pop())
